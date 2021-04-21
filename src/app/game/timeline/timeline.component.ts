ce62980dd4b7716fb4afc97e1119ee2ed787d78f
Mauranne Lagneau <mauranne.lagneau@gmail.com>
2021-04-21 11:35:27 +0200

    adding the first steps to add empty space, need to fix the last space of the right side

--- src/app/game/timeline/timeline.component.ts
+++ src/app/game/timeline/timeline.component.ts
@@ -7,6 +7,15 @@ import { Card } from "../../common/card.model";
   styleUrls: ["./timeline.component.css"],
 })
 export class TimelineComponent implements OnInit {
+  public timelineDeck: Card[] = [
+    { title: "Début de la 2nde guerre mondiale", date: 1939, img: "" },
+    { title: "Election de Nicolas Sarkozy", date: 2007, img: "" },
+    { title: "Premiers pas sur la lune", date: 1969, img: "" },
+    { title: "Révolution française", date: 1789, img: "" },
+  ];
+
+  public lastItem: boolean = false;
+
   constructor() {}
 
   @Input() firstCard: Card;
